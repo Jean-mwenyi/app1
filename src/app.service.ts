7ce194521d1c47d7490e10702b987c85b39c3798
jean-mwenyi <menyimalijean@gmail.com>
2024-03-15 18:24:56 +0100

    Correction

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World ! et test du temps execution du build de imageDocker';
+    return 'Hello World ! et test du temps execution du push de imageDocker';
   }
 }
